101acd4f6b8c4504a9d063414885c87a4c1cf6e5
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2021-04-23 08:29:13 -0300

    srbench

--- bin/itea-srbench.sh
+++ bin/itea-srbench.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 conda activate srbench
+
+if [ ! -f bin/libgsl.so.0 ]; then
+  cp $CONDA_PREFIX/lib/libgsl.so bin/libgsl.so.0
+fi
+
 export LD_LIBRARY_PATH=~/.conda/envs/srbench/lib:$LD_LIBRARY_PATH:$PWD/bin
 bin/itea-srbench $1 $2
