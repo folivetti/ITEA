d034fd135caa29911e306906b946388e417c9837
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2021-04-23 09:08:47 -0300

    adding libgsl.so.0 manually

--- bin/itea-srbench.sh
+++ bin/itea-srbench.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-source ~/.bashrc
 conda activate srbench
 
 if [ ! -f bin/libgsl.so.0 ]; then
   cp $CONDA_PREFIX/lib/libgsl.so bin/libgsl.so.0
 fi
 
-export LD_LIBRARY_PATH=~/.conda/envs/srbench/lib:$LD_LIBRARY_PATH:$PWD/bin
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH:$PWD/bin
 bin/itea-srbench $1 $2
